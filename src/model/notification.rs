cfab2787181ea8a1f1b166d04110de0e0eec3f0d
Fahmi Ramadhan <51454564+fahmi-ramadhan@users.noreply.github.com>
2024-04-15 14:26:23 +0700

    Implement update method in Subscriber model to send notification HTTP requests.

--- src/model/notification.rs
+++ src/model/notification.rs
@@ -4,7 +4,7 @@ use rocket::serde::{Deserialize, Serialize};
 #[serde(crate = "rocket::serde")]
 pub struct Notification {
     pub product_title: String,
-    pub product_tupe: String,
+    pub product_type: String,
     pub product_url: String,
     pub subscriber_name: String,
     pub status: String
